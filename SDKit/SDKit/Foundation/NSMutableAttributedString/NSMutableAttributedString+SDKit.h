b14129feb1e55c0710104fbd5d9149febac3ef3d
HRongChen <gavin.chen.hr@gmail.com>
2016-04-20 21:45:17 +0800

    添加一个多文本分段自定义带间距的NSMutableAttributedString方法

--- SDKit/SDKit/Foundation/NSMutableAttributedString/NSMutableAttributedString+SDKit.h
+++ SDKit/SDKit/Foundation/NSMutableAttributedString/NSMutableAttributedString+SDKit.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+
 @interface NSMutableAttributedString (SDKit)
+/**
+ *
+ *   实例化NSMutableAttributedString字符串
+ *
+ *  @param texts      用于存储所需设置字符串的数组(根据所需自定义类型分组)
+ *  @param attrsArray 富文本属性样式的集合(数组元素是texts的相应文本的属性--字典)
+ *  @param space      texts文本之间的间距数组 1 代表一个空格的距离
+ *
+ *  @return NSMutableAttributedString实例
+ */
+
+- (instancetype)initWithStrings:(NSArray <NSString *>*)texts attributesArray:(NSArray <NSDictionary<NSString *,id>*>*)attrsArray withSpace:(NSArray <NSNumber *>*)space;
 
 @end
