9b52072da3a40bfcebfd98d6c0a38eb2c93f7f45
boai <sunboyan@outlook.com>
2016-04-22 13:06:53 +0800

    新增Log类，打印统一用SDLog

--- SDKit/Others/AppDelegate.m
+++ SDKit/Others/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "SDLog.h"
 #import "SDKitDemoHomeViewController.h"
 
 @interface AppDelegate ()
@@ -93,7 +93,7 @@
         error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
         // Replace this with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        SDLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
     
@@ -125,7 +125,7 @@
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
             // Replace this implementation with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            SDLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
         }
     }
